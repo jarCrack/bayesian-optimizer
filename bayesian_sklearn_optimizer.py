90a16eef5fa6ed1dafb57c3f0ac1788a297668bb
Joern Malich <jmalich2006@googlemail.com>
2016-08-26 17:22:13 +0200

    Bayesian sklearn optimizer first protoype :)

--- bayesian_sklearn_optimizer.py
+++ bayesian_sklearn_optimizer.py
@@ -3,9 +3,9 @@ import GPyOpt
 import numpy as np
 from sklearn import svm
 from numpy.random import seed
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 from sklearn.cross_validation import train_test_split
-
+from sklearn.metrics import mean_absolute_error
 seed(12345)
 
 
@@ -13,11 +13,17 @@ class BayesianSkLearnOptimizer:
 
     def evaluate(self,hyperparams):
         print(hyperparams)
-        trained_model=self.model()
-        # self.model=self.model()
-        return 1.
+        hyps={c:np.exp(hyperparams[0,i]) for i,c in enumerate(self.hyp_names)}
+        self.trained_model=self.model(**hyps)
+        self.trained_model.fit(self.X_train,self.y_train)
 
-    def __init__(self, sklearn_model, params_to_tune, training_and_test_data, custom_validation=None):
+        plt.plot(self.X_test, self.y_test, 'x')
+        plt.plot(self.X_test, self.trained_model.predict(self.X_test), '.')
+        plt.show()
+
+        return mean_absolute_error(self.y_test,self.trained_model.predict(self.X_test))
+        #return self.trained_model.score(self.X_test,self.y_test)
+    def __init__(self, sklearn_model, params_to_tune, training_and_test_data, custom_validation=None,iteration_callback=None):
         """
         Initialization of the Bayesian sklearn model optimizer.
 
@@ -26,14 +32,22 @@ class BayesianSkLearnOptimizer:
         :param training_and_test_data:
         :param custom_validation: Custom validation function, if your need some nifty cross validation stuff :)
         """
+
         self.hyperparameters = params_to_tune
+        self.hyp_names=[x["name"] for x in self.hyperparameters]
+        self.X_train, self.X_test, self.y_train, self.y_test = training_and_test_data
         self.model = sklearn_model
+        self.iteration_callback=iteration_callback
+
         validation_method = custom_validation if callable(custom_validation) else lambda x:self.evaluate(x)
 
-        problem = GPyOpt.methods.BayesianOptimization(f=validation_method, domain=self.hyperparameters,
+        self.problem = GPyOpt.methods.BayesianOptimization(f=validation_method, domain=self.hyperparameters,
                                                       acquisition_type="LCB", acquisition_weight=0.1)
 
         print(hyperparameters)
+    def optimize_model(self):
+        self.problem.run_optimization(max_iter=10)
+        return self.trained_model
 
 if __name__ == "__main__":
     sklearn_model = svm.SVR
@@ -47,5 +61,11 @@ if __name__ == "__main__":
     y = data['Y']
 
     data = train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = data
 
     optimizer = BayesianSkLearnOptimizer(sklearn_model, hyperparameters, data)
+    model=optimizer.optimize_model()
+
+    plt.plot(X_test, y_test, 'x')
+    plt.plot(X_test, model.predict(X_test), '.')
+    plt.show()
